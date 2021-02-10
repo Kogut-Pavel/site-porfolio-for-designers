f120bbcf36ed079b11bada1fcad90c244fc1fa10
Kogut-Pavel <73947945+Kogut-Pavel@users.noreply.github.com>
2021-02-11 02:48:24 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.header-burger').click(function(event) {
+    $('.header-burger,.header-menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+  $('.header-menu').click(function(){
+    $('.header-burger, .header-menu').removeClass('active');
+    $('body').removeClass('lock');
+});
+});
+
